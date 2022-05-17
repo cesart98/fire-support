3c5e6a580776bf0ab650affc4efb0e3b79686c20
Cesar Torres <torres-cesar@outlook.com>
2022-05-17 15:56:03 -0500

    Add squares into board component

---
+++ src/Board.js
@@ -0,0 +1,133 @@
+function Board() {
+  function Square() {
+    return (
+      <div className="square"></div>
+    );
+  }
+  return (
+    <div className="Board">
+      <div className="board-row">
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+      <div className="board-row">
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+      <div className="board-row">
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+      <div className="board-row">
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+      <div className="board-row">
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+      <div className="board-row">
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+      <div className="board-row">
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+      <div className="board-row">
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+      <div className="board-row">
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+      <div className="board-row">
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+    </div>
+  );
+}
+
+export default Board;
