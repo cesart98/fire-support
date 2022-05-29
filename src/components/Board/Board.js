a676c502753651ed5d478da4621f805762c12cf0
Cesar Torres <torres-cesar@outlook.com>
2022-05-28 20:58:16 -0500

    Render square elements into board

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,124 +1,74 @@
 import './Board.css';
+import React, { useState, useEffect } from "react";
+import BoardPiece from '../Ship/Ship.js'
+
+const handleDragAndDrop = (mousedownEvent, elementArray) => {
+  const mouse = (() => {
+      const moveElem = (pageX, pageY) => {
+          elementContainer.style.left = pageX - shiftX + 'px';
+          elementContainer.style.top = pageY - shiftY + 'px';
+      }
+      const grabElem = (event) => {
+          elementContainer.style.position = 'absolute';
+          elementContainer.style.zIndex = 1000;
+          elementContainer.style.width = originalContainer.scrollWidth / 10 + 'px';
+          document.body.append(elementContainer);
+
+          mouse.moveElem(event.pageX, event.pageY);
+      }
+      const dropElem = () => {
+          elementContainer.style.position = 'static';
+          elementContainer.style.zIndex = 'auto';
+          elementContainer.style.width = 'auto;'
+      }
+      return { moveElem, grabElem, dropElem };
+  })();
+
+  let elementContainer = document.createElement('div');
+  let originalContainer;
+  elementArray.forEach(element => elementContainer.appendChild(element));
+
+  let shiftX = mousedownEvent.clientX - mousedownEvent.target.getBoundingClientRect().left;
+  let shiftY = mousedownEvent.clientY - mousedownEvent.target.getBoundingClientRect().top;
+  elementContainer.ondragstart = () => false;
+
+  // (1) grab elementContainer on mouse down
+  mouse.grabElem(mousedownEvent);
+
+  // (2) move elementContainer as mouse moves
+  document.onmousemove = (mousemoveEvent) => {
+      mouse.moveElem(mousemoveEvent.pageX, mousemoveEvent.pageY);
+  }
+  
+  // (3) drop elementContainer on mouse up,
+  elementContainer.onmouseup = () => {
+      document.onmousemove = null;
+      mouse.onmouseup = null;
+  } 
+}
 
 function Square(props) {
   return (
-    <div
-      className="square" 
-      id={props.id}
-    ></div>
+  <div id={props.id} className='board-square'>
+  </div>
   );
 }
 
-function Board() {
-  function renderSquare(i) {
-    return (
-      <Square id={i}/>
+export default function Board(props) {  
+  const [squareElements, setSquareElements] = useState([]);
+
+  useEffect(function updateSquareElements() {
+    let newSquareElements = props.squares.map((data, index) =>
+      <Square id={index} key={index.toString()}>
+
+      </Square>
     );
-  }
+    setSquareElements(newSquareElements);
+  },[props.squares],);
+
   return (
     <div className="Board">
-      {renderSquare(0)}
-      {renderSquare(1)}
-      {renderSquare(2)}
-      {renderSquare(3)}
-      {renderSquare(4)}
-      {renderSquare(5)}
-      {renderSquare(6)}
-      {renderSquare(7)}
-      {renderSquare(8)}
-      {renderSquare(9)}
-      {renderSquare(10)}
-      {renderSquare(11)}
-      {renderSquare(12)}
-      {renderSquare(13)}
-      {renderSquare(14)}
-      {renderSquare(15)}
-      {renderSquare(16)}
-      {renderSquare(17)}
-      {renderSquare(18)}
-      {renderSquare(19)}
-      {renderSquare(20)}
-      {renderSquare(21)}
-      {renderSquare(22)}
-      {renderSquare(23)}
-      {renderSquare(24)}
-      {renderSquare(25)}
-      {renderSquare(26)}
-      {renderSquare(27)}
-      {renderSquare(28)}
-      {renderSquare(29)}
-      {renderSquare(30)}
-      {renderSquare(31)}
-      {renderSquare(32)}
-      {renderSquare(33)}
-      {renderSquare(34)}
-      {renderSquare(35)}
-      {renderSquare(36)}
-      {renderSquare(37)}
-      {renderSquare(38)}
-      {renderSquare(39)}
-      {renderSquare(40)}
-      {renderSquare(41)}
-      {renderSquare(42)}
-      {renderSquare(43)}
-      {renderSquare(44)}
-      {renderSquare(45)}
-      {renderSquare(46)}
-      {renderSquare(47)}
-      {renderSquare(48)}
-      {renderSquare(49)}
-      {renderSquare(50)}
-      {renderSquare(51)}
-      {renderSquare(52)}
-      {renderSquare(53)}
-      {renderSquare(54)}
-      {renderSquare(55)}
-      {renderSquare(56)}
-      {renderSquare(57)}
-      {renderSquare(58)}
-      {renderSquare(59)}
-      {renderSquare(60)}
-      {renderSquare(61)}
-      {renderSquare(62)}
-      {renderSquare(63)}
-      {renderSquare(64)}
-      {renderSquare(65)}
-      {renderSquare(66)}
-      {renderSquare(67)}
-      {renderSquare(68)}
-      {renderSquare(69)}
-      {renderSquare(70)}
-      {renderSquare(71)}
-      {renderSquare(72)}
-      {renderSquare(73)}
-      {renderSquare(74)}
-      {renderSquare(75)}
-      {renderSquare(76)}
-      {renderSquare(77)}
-      {renderSquare(78)}
-      {renderSquare(79)}
-      {renderSquare(80)}
-      {renderSquare(81)}
-      {renderSquare(82)}
-      {renderSquare(83)}
-      {renderSquare(84)}
-      {renderSquare(85)}
-      {renderSquare(86)}
-      {renderSquare(87)}
-      {renderSquare(88)}
-      {renderSquare(89)}
-      {renderSquare(90)}
-      {renderSquare(91)}
-      {renderSquare(92)}
-      {renderSquare(93)}
-      {renderSquare(94)}
-      {renderSquare(95)}
-      {renderSquare(96)}
-      {renderSquare(97)}
-      {renderSquare(98)}
-      {renderSquare(99)}
+      {squareElements}
     </div>
   );
 }
-
-export default Board;
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,23 +1,31 @@
-import Board from '../Board/Board.js'
-import Ship from '../Ship/Ship.js'
 import './Game.css';
+import Board from '../Board/Board.js'
+import React, { useState, useEffect } from "react";
 
-function Game() {
+export default function Game() {
+  const [squares, setSquaress] = useState(Array(100).fill(
+    { hasShip: false, isHit: false }
+  ));
+  const [pieces, setPieces] = useState([
+    { name: 'carrier', length: 5, locations: [21, 22, 23, 24, 25] },
+    { name: 'battleship', length: 4, locations: [3, 4, 5, 6] },
+    { name: 'cruiser', length: 3, locations: [51, 52, 53] },
+    { name: 'submarine', length: 2, locations: [55, 56] },
+    { name: 'submarine', length: 2, locations: [75, 76] },
+    { name: 'tugboat', length: 1, locations: [84] },
+    { name: 'tugboat', length: 1, locations: [88] },
+  ]);
+  const [phase, setPhase] = useState('prep');
+
+
   return (
     <div className='Game'>
-      <div className='board-container'>
-        <Board></Board>
-      </div>
-      <div className='ship-container'>
-        <Ship></Ship>
-        <Ship></Ship>
-        <Ship></Ship>
-        <Ship></Ship>
-        <Ship></Ship>
-        <Ship></Ship>
+      <div className='game-title'>Fire Support</div>
+      <div className='game-instructions'>Arrange your vehicles</div>
+      <div className='game-ui'>
+        <Board squares={squares}/>
+        <button className='game-action' />
       </div>
     </div>
   );
 }
-
-export default Game;
